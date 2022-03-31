6594db1132e53b06ff451c425ffc51741484ba90
stevenolido <102740113+stevenolido@users.noreply.github.com>
2022-03-31 23:23:16 +0500

    Update views.py

--- banks/views.py
+++ banks/views.py
@@ -1,8 +1,6 @@
 from rest_framework import mixins, serializers, viewsets
 
 from banks import models
-from programs import models as pmodel
-import pycountry
 
 
 class BankSerializer(serializers.ModelSerializer):
@@ -18,16 +16,8 @@ class BankViewSet(viewsets.ModelViewSet):
 
 class TransactionSerializer(serializers.Serializer):
     is_eligible = serializers.BooleanField(default=True)
-    bank_name =  serializers.CharField()
-    program_name = serializers.CharField()
-    country_name = serializers.CharField()
 
     def create(self, validated_data):
-        country = pycountry.countries.get(name='Pakistan')
-        currency = pycountry.currencies.get(numeric=country.numeric)
-        pname = pmodel.Program.objects.get(name = program_name)
-        print(pname) 
-
         pass
 
     def update(self, instance, validated_data):
@@ -41,7 +31,5 @@ class TransactionViewSet(
     serializer_class = TransactionSerializer
 
     def perform_create(self, serializer):
-        print(serializer_class.pname)
-        
         pass
 
