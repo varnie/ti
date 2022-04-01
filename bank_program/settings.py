f3f39ba759c667c951b2c0cfedde5ef3b50c215a
varnie <varnie29a@mail.ru>
2022-04-01 17:50:12 +0500

    settings please

--- bank_program/settings.py
+++ bank_program/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'banks',
     'programs',
+    'associations',
+    'transaction',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
